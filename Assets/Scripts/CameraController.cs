e7db8dd68f5ea49eb745cf122243013ca10c5f7c
Zoee Silcock <mrzoee@gmail.com>
2015-07-24 18:28:22 +0200

    Camera follows the player

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject _player;
+    public float _offsetX;
+    public float _offsetZ;
+    public float _smoothSpeed;
+
+    void Update() {
+        Vector3 target = new Vector3(_player.transform.position.x + _offsetX, transform.position.y, _player.transform.position.z + _offsetZ);
+        transform.position = Vector3.Lerp(transform.position, target, _smoothSpeed * Time.deltaTime);
+    }
+}
