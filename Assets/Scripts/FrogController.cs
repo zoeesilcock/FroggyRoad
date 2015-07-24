f116cb76a4ae782ccc67af4f64792f26c2803835
Zoee Silcock <mrzoee@gmail.com>
2015-07-24 18:39:05 +0200

    Only allow jumping when the player is on the ground

--- Assets/Scripts/FrogController.cs
+++ Assets/Scripts/FrogController.cs
@@ -6,9 +6,23 @@ public class FrogController : MonoBehaviour {
     public float _jumpForceY;
     public float _jumpForceX;
 
+    private bool grounded;
+
     void Update() {
-        if (Input.GetMouseButtonDown(0)) {
+        if (Input.GetMouseButtonDown(0) && grounded) {
             gameObject.GetComponent<Rigidbody>().AddForce(_jumpForceX, _jumpForceY, 0.0f);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        if (collision.collider.CompareTag("Ground")) {
+            grounded = true;
+        }
+    }
+
+    void OnCollisionExit(Collision collision) {
+        if (collision.collider.CompareTag("Ground")) {
+            grounded = false;
         }
     }
 }
