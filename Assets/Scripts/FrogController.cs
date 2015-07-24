3c1afd6dcbc71eba71fef2453f26777d71d591f6
Zoee Silcock <mrzoee@gmail.com>
2015-07-24 18:07:35 +0200

    Add player with a basic jump

---
+++ Assets/Scripts/FrogController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FrogController : MonoBehaviour {
+
+    public float _jumpForceY;
+    public float _jumpForceX;
+
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            gameObject.GetComponent<Rigidbody>().AddForce(_jumpForceX, _jumpForceY, 0.0f);
+        }
+    }
+}
