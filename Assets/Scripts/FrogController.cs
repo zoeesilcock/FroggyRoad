faf3e6b38363d70d694ab285768bd70a23f93570
Zoee Silcock <mrzoee@gmail.com>
2015-07-24 19:23:07 +0200

    Add jumps in all directions

--- Assets/Scripts/FrogController.cs
+++ Assets/Scripts/FrogController.cs
@@ -3,15 +3,33 @@ using System.Collections;
 
 public class FrogController : MonoBehaviour {
 
-    public float _jumpForceY;
-    public float _jumpForceX;
+    public float _jumpForceUp;
+    public float _jumpForceForward;
+    public float _smoothTurn;
 
     private bool grounded;
+    private float targetRotationY;
 
     void Update() {
-        if (Input.GetMouseButtonDown(0) && grounded) {
-            gameObject.GetComponent<Rigidbody>().AddForce(_jumpForceX, _jumpForceY, 0.0f);
+        if (Input.GetAxis("Vertical") > 0.0f && grounded) {
+            Jump(_jumpForceForward, 0, 0);
+        } else if (Input.GetAxis("Vertical") < 0.0f && grounded) {
+            Jump(-_jumpForceForward, 0, 180);
+        } else if (Input.GetAxis("Horizontal") < 0.0f && grounded) {
+            Jump(0, _jumpForceForward, -90);
+        } else if (Input.GetAxis("Horizontal") > 0.0f && grounded) {
+            Jump(0, -_jumpForceForward, 90);
         }
+
+        if (transform.rotation.y != targetRotationY) {
+            Quaternion target = Quaternion.Euler(270, targetRotationY, 0);
+            transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * _smoothTurn);
+        }
+    }
+
+    void Jump(float forceX, float forceZ, float rotation) {
+        targetRotationY = rotation;
+        gameObject.GetComponent<Rigidbody>().AddForce(forceX, _jumpForceUp, forceZ);
     }
 
     void OnCollisionEnter(Collision collision) {
