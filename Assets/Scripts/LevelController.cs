892dd2abc5e68afcfae5bf0487afc58433b5916e
Zoee Silcock <mrzoee@gmail.com>
2015-07-27 23:37:48 +0200

    Generate level sections randomly

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -6,11 +6,16 @@ public class LevelController : MonoBehaviour {
 
     public GameObject _player;
     public GameObject _grassPrefab;
+    public GameObject _roadPrefab;
     public float _spaceForwards;
     public float _spaceBackwards;
+    public int _maxGrassCount;
+    public int _maxRoadCount;
 
     private Transform firstLevelSection;
     private Transform lastLevelSection;
+    private int grassCount;
+    private int roadCount;
 
     void Start() {
     }
@@ -33,7 +38,26 @@ public class LevelController : MonoBehaviour {
     }
 
     void GenerateNewSection() {
-        GameObject section = Instantiate(_grassPrefab, new Vector3(lastLevelSection.position.x + 2, lastLevelSection.position.y, lastLevelSection.position.z), Quaternion.Euler(270, 0, 0)) as GameObject;
+        GameObject prefab = _grassPrefab;
+
+        if (Random.value > 0.75) {
+            prefab = _roadPrefab;
+            roadCount += 1;
+        } else {
+            grassCount += 1;
+        }
+
+        if (grassCount >= _maxGrassCount) {
+            prefab = _roadPrefab;
+            grassCount = 0;
+        }
+
+        if (roadCount >= _maxRoadCount) {
+            prefab = _grassPrefab;
+            roadCount = 0;
+        }
+
+        GameObject section = Instantiate(prefab, new Vector3(lastLevelSection.position.x + 2, lastLevelSection.position.y, lastLevelSection.position.z), Quaternion.Euler(270, 0, 0)) as GameObject;
         section.transform.parent = transform;
         IdentifyFirstAndLast();
     }
