ac3a231241aef2d095f1500c14e74f430e3ce82d
Zoee Silcock <mrzoee@gmail.com>
2015-07-27 21:36:42 +0200

    Remove cars once they have left the edge of the level

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -8,4 +8,10 @@ public class CarController : MonoBehaviour {
     void Start() {
         transform.GetComponent<Rigidbody>().velocity = new Vector3(0.0f, 0.0f, _speed);
     }
+
+    void Update() {
+        if (transform.position.z < -10.0f || transform.position.z > 10.0f) {
+            Destroy(gameObject);
+        }
+    }
 }
