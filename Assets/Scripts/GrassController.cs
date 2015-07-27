5ee95e19b7c94c4bf18adf856962a117d47cc4e8
Zoee Silcock <mrzoee@gmail.com>
2015-07-28 01:02:12 +0200

    Add disco grass as a future easter egg

---
+++ Assets/Scripts/GrassController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrassController : MonoBehaviour {
+
+    void Update() {
+        /* Renderer grassRenderer = gameObject.GetComponent<Renderer>(); */
+        /* grassRenderer.material.color = new Color(Random.value, Random.value, Random.value); */
+    }
+}
