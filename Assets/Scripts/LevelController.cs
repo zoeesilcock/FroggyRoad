8df6499961880b24112623ba6f473f6a9e43aed5
Zoee Silcock <mrzoee@gmail.com>
2015-07-27 23:01:26 +0200

    Generate new grass sections and remove old sections

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class LevelController : MonoBehaviour {
+
+    public GameObject _player;
+    public GameObject _grassPrefab;
+    public float _spaceForwards;
+    public float _spaceBackwards;
+
+    private Transform firstLevelSection;
+    private Transform lastLevelSection;
+
+    void Start() {
+    }
+
+    void Update() {
+        IdentifyFirstAndLast();
+
+        if (_player.transform.position.x > firstLevelSection.position.x + _spaceBackwards) {
+            Destroy(firstLevelSection.gameObject);
+        }
+
+        if (_player.transform.position.x + _spaceForwards > lastLevelSection.position.x) {
+            GenerateNewSection();
+        }
+    }
+
+    void IdentifyFirstAndLast() {
+        firstLevelSection = gameObject.GetComponentsInChildren<Transform>().Skip(1).OrderBy(t => t.position.x).First();
+        lastLevelSection = gameObject.GetComponentsInChildren<Transform>().Skip(1).OrderBy(t => t.position.x).Last();
+    }
+
+    void GenerateNewSection() {
+        GameObject section = Instantiate(_grassPrefab, new Vector3(lastLevelSection.position.x + 2, lastLevelSection.position.y, lastLevelSection.position.z), Quaternion.Euler(270, 0, 0)) as GameObject;
+        section.transform.parent = transform;
+        IdentifyFirstAndLast();
+    }
+}
