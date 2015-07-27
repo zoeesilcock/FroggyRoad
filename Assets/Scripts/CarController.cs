4f0156f64dd292f7e7e686c466be7ba52d26abd5
Zoee Silcock <mrzoee@gmail.com>
2015-07-27 19:51:52 +0200

    Move the car

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarController : MonoBehaviour {
+
+    public float _speed;
+
+    void Start() {
+        transform.GetComponent<Rigidbody>().velocity = new Vector3(0.0f, 0.0f, _speed);
+    }
+}
