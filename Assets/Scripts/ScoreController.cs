f853ba4026e0cafb18e3e16e4e0a9b6e7cf10053
Zoee Silcock <mrzoee@gmail.com>
2015-07-28 19:42:00 +0200

    Display a score based on how far forward the player has moved

---
+++ Assets/Scripts/ScoreController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreController : MonoBehaviour {
+
+    public GameObject _player;
+    public float _startPosition;
+
+    void Start() {
+    }
+
+    void Update() {
+        gameObject.GetComponent<Text>().text = "Points: " + Mathf.Floor(_player.transform.position.x - _startPosition);
+    }
+}
